1d543d5908b1b5b45b22a9fa7f0799b94c394ff1
David Simon <dsimon@americancouncils.org>
2013-09-03 16:43:33 -0400

    Subresources

--- src/services/resourceMock.js
+++ src/services/resourceMock.js
@@ -4,25 +4,56 @@ angular.module('restfulNgMock')
 .factory('resourceMock', [
 '$httpBackend',
 function($httpBackend) {
+  var buildJsonResponse = function(data, code) {
+    code = code || 200;
+    return [
+      code,
+      JSON.stringify(data),
+      {
+        'Content-Type': 'application/json'
+      }
+    ];
+  };
+
+  var buildJsonErrorResponse = function(code, message) {
+    var data = {
+      code: code,
+      message: message
+    };
+    return buildJsonResponse(data, code);
+  };
+
   var ResourceMock = function (baseUrl, dataSource) {
-    if (!(/^\/[\w\/\-]+\w$/).test(baseUrl)) {
+    if (!(/^\/[\w\-]+(\/[\w\-]+|\/\?)*$/).test(baseUrl)) {
       throw 'Invalid baseUrl for resourceMock: "' + baseUrl + '".';
     }
 
     this.baseUrl = baseUrl;
-    this.baseUrlRe = new RegExp('^' + baseUrl.replace('/', '\\/', 'g') + '(?:/([\\w\\-]+))?$');
+    this.dataSource = dataSource;
+
+    this.requiredSegments = 0;
+    for (var cidx = 0; cidx < baseUrl.length; ++cidx) {
+      if (baseUrl.charAt(cidx) === '?') { ++this.requiredSegments; }
+    }
+
+    var urlPattern = baseUrl
+      .replace('/', '\\/', 'g')
+      .replace('?', '([\\w\\-]+)');
+    this.baseUrlRe = new RegExp( '^' + urlPattern  + '(?:/([\\w\\-]+))?$');
 
     var me = this;
 
     $httpBackend.whenGET(new RegExp(this.baseUrlRe))
     .respond(function(method, rawUrl, data, headers) {
       return me.handle(rawUrl, data, headers, {
-        atRoot: function() {
-          return me.jsonResponse(dataSource);
+        atRoot: function(ids) {
+          var storage = me.getStorage(ids);
+          if (storage) { return buildJsonResponse(storage); }
         },
-        atItem: function(itemId) {
-          if (dataSource[itemId]) {
-            return me.jsonResponse(dataSource[itemId]);
+        atItem: function(superIds, itemId) {
+          var storage = me.getStorage(superIds);
+          if (storage && storage[itemId]) {
+            return buildJsonResponse(storage[itemId]);
           }
         }
       });
@@ -31,11 +62,11 @@ function($httpBackend) {
     $httpBackend.whenPOST(new RegExp(this.baseUrlRe))
     .respond(function(method, rawUrl, data, headers) {
       return me.handle(rawUrl, data, headers, {
-        atRoot: function() {
+        atRoot: function(ids) {
           var newItem = JSON.parse(data);
           newItem.id = Math.round(Math.random()*Math.pow(2, 32)).toString();
-          dataSource[newItem.id] = newItem;
-          return me.jsonResponse(newItem);
+          me.getStorage(ids, true)[newItem.id] = newItem;
+          return buildJsonResponse(newItem);
         }
       });
     });
@@ -43,12 +74,13 @@ function($httpBackend) {
     $httpBackend.whenPUT(new RegExp(this.baseUrlRe))
     .respond(function(method, rawUrl, data, headers) {
       return me.handle(rawUrl, data, headers, {
-        atItem: function(itemId) {
-          if (dataSource[itemId]) {
+        atItem: function(superIds, itemId) {
+          var storage = me.getStorage(superIds);
+          if (storage && storage[itemId]) {
             var newItem = JSON.parse(data);
             newItem.id = itemId;
-            dataSource[itemId] = newItem;
-            return me.jsonResponse(newItem);
+            storage[itemId] = newItem;
+            return buildJsonResponse(newItem);
           }
         }
       });
@@ -57,9 +89,10 @@ function($httpBackend) {
     $httpBackend.whenDELETE(new RegExp(this.baseUrlRe))
     .respond(function(method, rawUrl, data, headers) {
       return me.handle(rawUrl, data, headers, {
-        atItem: function(itemId) {
-          if (dataSource[itemId]) {
-            delete dataSource[itemId];
+        atItem: function(superIds, itemId) {
+          var storage = me.getStorage(superIds);
+          if (storage && storage[itemId]) {
+            delete storage[itemId];
           }
         }
       });
@@ -69,42 +102,50 @@ function($httpBackend) {
   ResourceMock.prototype = {
     handle: function(rawUrl, data, headers, handlers) {
       var url = purl(rawUrl);
+      var matches = this.baseUrlRe.exec(url.attr('path')).slice(1);
+      var itemIds = [];
+      for (var i = 0; i < matches.length; ++i) {
+        if (typeof matches[i] !== 'null' && typeof matches[i] !== 'undefined') {
+          itemIds.push(matches[i]);
+        }
+      }
+
       var result;
 
-      if (handlers.atRoot && url.attr('path') === this.baseUrl) {
-        result = handlers.atRoot(url, data, headers);
+      if (handlers.atRoot && itemIds.length === this.requiredSegments) {
+        result = handlers.atRoot(itemIds, url, data, headers);
+        if (result) { return result; }
+      } else if (handlers.atItem && itemIds.length > this.requiredSegments) {
+        var superIds = itemIds.slice(0, -1);
+        var itemId = itemIds[itemIds.length-1];
+        result = handlers.atItem(superIds, itemId, url, data, headers);
         if (result) { return result; }
       }
 
-      if (handlers.atItem) {
-        var match = this.baseUrlRe.exec(url.attr('path'));
-        if (match) {
-          result = handlers.atItem(match[1], url, data, headers);
-          if (result) { return result; }
+      return buildJsonErrorResponse(404, 'Not Found');
+    },
+
+    getStorage: function(ids, autoCreate) {
+      autoCreate = autoCreate || false;
+      var d = this.dataSource;
+      for (var i = 0; i < ids.length; ++i) {
+        if (d[ids[i]]) {
+          d = d[ids[i]];
+        } else {
+          if (autoCreate) {
+            d[ids[i]] = {};
+            d = d[ids[i]];
+          } else {
+            return null;
+          }
         }
       }
-
-      return this.jsonErrorResponse(404, 'Not Found');
+      return d || null;
     },
 
-    jsonResponse: function(data, code) {
-      code = code || 200;
-      return [
-        code,
-        JSON.stringify(data),
-        {
-          'Content-Type': 'application/json'
-        }
-      ];
+    subResourceMock: function(subUrl, subDataSource) {
+      return new ResourceMock(this.baseUrl + '/?' + subUrl, subDataSource);
     },
-
-    jsonErrorResponse: function(code, message) {
-      var data = {
-        code: code,
-        message: message
-      };
-      return this.jsonResponse(data, code);
-    }
   };
 
   var ResourceMockFactory = function(baseUrl, dataSource) {
---
+++ test/unit/resourceMockSpec.js
@@ -0,0 +1,248 @@
+'use strict';
+
+describe('resourceMock', function () {
+  beforeEach(module('restfulNgMock'));
+
+  var resourceMock, $http, $httpBackend;
+  beforeEach(inject(function(_resourceMock_, _$http_, _$httpBackend_) {
+    resourceMock = _resourceMock_;
+    $http = _$http_;
+    $httpBackend = _$httpBackend_;
+  }));
+
+  var METHODS = ['GET', 'POST', 'PUT', 'DELETE'];
+
+  var result;
+  function grabHttpResult(h) {
+    var grab = function(d, s, h, c) {
+      result = {};
+      result.data = d;
+      result.status = s;
+      result.headers = h;
+      result.config = c;
+    };
+    h.success(grab).error(grab);
+  }
+
+  describe('when used incorrectly', function () {
+    it('fails when given template URL without starting slash', function () {
+      expect(function() {
+        resourceMock("foo");
+      }).toThrow();
+    });
+
+    it('fails when given template URL with closing slash', function () {
+      expect(function() {
+        resourceMock("/foo/"); // Shouldn't end with a slash
+      }).toThrow();
+    });
+
+    it('fails when given template URL with invalid character', function () {
+      expect(function() {
+        resourceMock("/fo&o"); // Invalid character
+      }).toThrow();
+    });
+  });
+
+  describe('with a simple resource', function () {
+    var books, booksMock;
+    beforeEach(function() {
+      books = {
+        '1' : {
+          id: '1',
+          title: "A Woman of the Iron People",
+          author: "Eleanor Arnason"
+        },
+        '2' : {
+          id: '2',
+          title: "Anathem",
+          author: "Neal Stephensen"
+        },
+        '3' : {
+          id: '3',
+          title: "The C++ Programming Language",
+          author: "Bjarne Stroustrup"
+        }
+      };
+
+      booksMock = resourceMock('/books', books);
+    });
+
+    it('returns the full list on a simple get', function () {
+      grabHttpResult($http.get('/books'));
+      $httpBackend.flush();
+      expect(result.status).toEqual(200);
+      expect(result.data).toEqual(books);
+    });
+
+    it('returns a single item by id', function () {
+      grabHttpResult($http.get('/books/2'));
+      $httpBackend.flush();
+      expect(result.status).toEqual(200);
+      expect(result.data).toEqual(books['2']);
+    });
+
+    it('creates an item', function () {
+      grabHttpResult($http.post('/books', {
+        title: 'Godel, Escher, Bach',
+        author: 'Douglas Hofstadter'
+      }));
+      $httpBackend.flush();
+      expect(result.status).toEqual(200);
+      expect(result.data.title).toEqual('Godel, Escher, Bach');
+      expect(result.data.author).toEqual('Douglas Hofstadter');
+      var newId = String(result.data.id);
+      expect(newId.length).toBeGreaterThan(0);
+      expect(newId).not.toEqual('1');
+      expect(newId).not.toEqual('2');
+      expect(newId).not.toEqual('3');
+      expect(result.data).toEqual(books[newId]);
+    });
+
+    it('updates an item', function () {
+      grabHttpResult($http.put('/books/2', {
+        title: 'Diamond Age',
+        author: 'Neal Stephensen'
+      }));
+      $httpBackend.flush();
+      expect(result.data.title).toEqual('Diamond Age');
+      expect(result.data.id).toEqual('2');
+      expect(result.data).toEqual(books['2']);
+    });
+
+    it('deletes an item', function () {
+      grabHttpResult($http.delete('/books/2'));
+      $httpBackend.flush();
+      expect(books[2]).not.toBeDefined();
+
+      grabHttpResult($http.get('/books/2'));
+      $httpBackend.flush()
+      expect(result.status).toEqual(404);
+    });
+
+    it('returns a 404 error if item not found', function () {
+      for (var i = 0; i < METHODS.length; ++i) {
+        if (METHODS[i] != 'POST') {
+          grabHttpResult($http[METHODS[i].toLowerCase()]('/books/22'));
+          $httpBackend.flush();
+          expect(result.status).toEqual(404);
+          expect(result.data).toEqual({
+            code: 404,
+            message: "Not Found"
+          });
+        }
+      }
+    });
+  });
+
+  describe('with nested resources', function () {
+    var stores, storesMock, foods, foodsMock;
+    beforeEach(function() {
+      stores = {
+        a: {
+          id: 'a',
+          name: 'Sticky Fingers Bakery'
+        },
+        b: {
+          id: 'b',
+          name: 'District of Pi'
+        }
+      };
+
+      foods = {
+        a: {
+          '1': {
+            id: '1',
+            name: 'Tempeh Lettuce Tomato Sandwich'
+          },
+          '2': {
+            id: '2',
+            name: 'Coconut Cupcake'
+          }
+        },
+        b: {
+          '3': {
+            id: '3',
+            name: 'East Loop Pi',
+          },
+          '4': {
+            id: '4',
+            name: 'Maplewood Pi',
+          },
+          '5': {
+            id: '5',
+            name: 'Lincoln Park Pi'
+          }
+        }
+      };
+
+      storesMock = resourceMock('/stores', stores);
+      foodsMock = storesMock.subResourceMock('/foods', foods);
+    });
+
+    it('returns the full list of subresources on a simple get', function () {
+      grabHttpResult($http.get('/stores/b/foods'));
+      $httpBackend.flush();
+      expect(result.status).toEqual(200);
+      expect(result.data).toEqual(foods['b']);
+    });
+
+    it('returns a single subitem by id', function () {
+      grabHttpResult($http.get('/stores/b/foods/3'));
+      $httpBackend.flush();
+      expect(result.status).toEqual(200);
+      expect(result.data).toEqual(foods['b']['3']);
+    });
+
+    it('creates a subitem', function () {
+      grabHttpResult($http.post('/stores/b/foods', {
+        name: 'Grove Pi'
+      }));
+      $httpBackend.flush();
+      expect(result.status).toEqual(200);
+      expect(result.data.name).toEqual('Grove Pi');
+      var newId = String(result.data.id);
+      expect(newId.length).toBeGreaterThan(0);
+      expect(newId).not.toEqual('1');
+      expect(newId).not.toEqual('2');
+      expect(newId).not.toEqual('3');
+      expect(newId).not.toEqual('4');
+      expect(newId).not.toEqual('5');
+      expect(result.data).toEqual(foods['b'][newId]);
+    });
+
+    it('updates a subitem', function () {
+      grabHttpResult($http.put('/stores/b/foods/4', {
+        name: 'Grove Pi'
+      }));
+      $httpBackend.flush();
+      expect(result.data.name).toEqual('Grove Pi');
+      expect(result.data.id).toEqual('4');
+      expect(result.data).toEqual(foods['b']['4']);
+    });
+
+    it('deletes a subitem', function () {
+      grabHttpResult($http.delete('/stores/b/foods/3'));
+      $httpBackend.flush();
+      expect(foods['b']['3']).not.toBeDefined();
+
+      grabHttpResult($http.get('/stores/b/foods/3'));
+      $httpBackend.flush()
+      expect(result.status).toEqual(404);
+    });
+
+    it('returns a 404 error if subitem not found', function () {
+      for (var i = 0; i < METHODS.length; ++i) {
+        if (METHODS[i] != 'POST') {
+          grabHttpResult($http[METHODS[i].toLowerCase()]('/stores/b/foods/22'));
+          $httpBackend.flush();
+          expect(result.status).toEqual(404);
+          expect(result.data).toEqual({
+            code: 404,
+            message: "Not Found"
+          });
+        }
+      }
+    });
+  });
+});
