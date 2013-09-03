fc366d7ee1f7451a477f014a4d1d601380f0d671
David Simon <dsimon@americancouncils.org>
2013-09-03 16:54:01 -0400

    Removing private methods from ResourceMock prototype

--- src/services/resourceMock.js
+++ src/services/resourceMock.js
@@ -27,82 +27,21 @@ function($httpBackend) {
     if (!(/^\/[\w\-]+(\/[\w\-]+|\/\?)*$/).test(baseUrl)) {
       throw 'Invalid baseUrl for resourceMock: "' + baseUrl + '".';
     }
-
     this.baseUrl = baseUrl;
-    this.dataSource = dataSource;
 
-    this.requiredSegments = 0;
+    var requiredSegments = 0;
     for (var cidx = 0; cidx < baseUrl.length; ++cidx) {
-      if (baseUrl.charAt(cidx) === '?') { ++this.requiredSegments; }
+      if (baseUrl.charAt(cidx) === '?') { ++requiredSegments; }
     }
 
     var urlPattern = baseUrl
       .replace('/', '\\/', 'g')
       .replace('?', '([\\w\\-]+)');
-    this.baseUrlRe = new RegExp( '^' + urlPattern  + '(?:/([\\w\\-]+))?$');
+    var baseUrlRe = new RegExp( '^' + urlPattern  + '(?:/([\\w\\-]+))?$');
 
-    var me = this;
-
-    $httpBackend.whenGET(new RegExp(this.baseUrlRe))
-    .respond(function(method, rawUrl, data, headers) {
-      return me.handle(rawUrl, data, headers, {
-        atRoot: function(ids) {
-          var storage = me.getStorage(ids);
-          if (storage) { return buildJsonResponse(storage); }
-        },
-        atItem: function(superIds, itemId) {
-          var storage = me.getStorage(superIds);
-          if (storage && storage[itemId]) {
-            return buildJsonResponse(storage[itemId]);
-          }
-        }
-      });
-    });
-
-    $httpBackend.whenPOST(new RegExp(this.baseUrlRe))
-    .respond(function(method, rawUrl, data, headers) {
-      return me.handle(rawUrl, data, headers, {
-        atRoot: function(ids) {
-          var newItem = JSON.parse(data);
-          newItem.id = Math.round(Math.random()*Math.pow(2, 32)).toString();
-          me.getStorage(ids, true)[newItem.id] = newItem;
-          return buildJsonResponse(newItem);
-        }
-      });
-    });
-
-    $httpBackend.whenPUT(new RegExp(this.baseUrlRe))
-    .respond(function(method, rawUrl, data, headers) {
-      return me.handle(rawUrl, data, headers, {
-        atItem: function(superIds, itemId) {
-          var storage = me.getStorage(superIds);
-          if (storage && storage[itemId]) {
-            var newItem = JSON.parse(data);
-            newItem.id = itemId;
-            storage[itemId] = newItem;
-            return buildJsonResponse(newItem);
-          }
-        }
-      });
-    });
-
-    $httpBackend.whenDELETE(new RegExp(this.baseUrlRe))
-    .respond(function(method, rawUrl, data, headers) {
-      return me.handle(rawUrl, data, headers, {
-        atItem: function(superIds, itemId) {
-          var storage = me.getStorage(superIds);
-          if (storage && storage[itemId]) {
-            delete storage[itemId];
-          }
-        }
-      });
-    });
-  };
-
-  ResourceMock.prototype = {
-    handle: function(rawUrl, data, headers, handlers) {
+    var handle = function(rawUrl, data, headers, handlers) {
       var url = purl(rawUrl);
-      var matches = this.baseUrlRe.exec(url.attr('path')).slice(1);
+      var matches = baseUrlRe.exec(url.attr('path')).slice(1);
       var itemIds = [];
       for (var i = 0; i < matches.length; ++i) {
         if (typeof matches[i] !== 'null' && typeof matches[i] !== 'undefined') {
@@ -112,10 +51,10 @@ function($httpBackend) {
 
       var result;
 
-      if (handlers.atRoot && itemIds.length === this.requiredSegments) {
+      if (handlers.atRoot && itemIds.length === requiredSegments) {
         result = handlers.atRoot(itemIds, url, data, headers);
         if (result) { return result; }
-      } else if (handlers.atItem && itemIds.length > this.requiredSegments) {
+      } else if (handlers.atItem && itemIds.length > requiredSegments) {
         var superIds = itemIds.slice(0, -1);
         var itemId = itemIds[itemIds.length-1];
         result = handlers.atItem(superIds, itemId, url, data, headers);
@@ -123,11 +62,11 @@ function($httpBackend) {
       }
 
       return buildJsonErrorResponse(404, 'Not Found');
-    },
+    };
 
-    getStorage: function(ids, autoCreate) {
+    var getStorage = function(ids, autoCreate) {
       autoCreate = autoCreate || false;
-      var d = this.dataSource;
+      var d = dataSource;
       for (var i = 0; i < ids.length; ++i) {
         if (d[ids[i]]) {
           d = d[ids[i]];
@@ -141,8 +80,65 @@ function($httpBackend) {
         }
       }
       return d || null;
-    },
+    };
 
+    $httpBackend.whenGET(new RegExp(baseUrlRe))
+    .respond(function(method, rawUrl, data, headers) {
+      return handle(rawUrl, data, headers, {
+        atRoot: function(ids) {
+          var storage = getStorage(ids);
+          if (storage) { return buildJsonResponse(storage); }
+        },
+        atItem: function(superIds, itemId) {
+          var storage = getStorage(superIds);
+          if (storage && storage[itemId]) {
+            return buildJsonResponse(storage[itemId]);
+          }
+        }
+      });
+    });
+
+    $httpBackend.whenPOST(new RegExp(baseUrlRe))
+    .respond(function(method, rawUrl, data, headers) {
+      return handle(rawUrl, data, headers, {
+        atRoot: function(ids) {
+          var newItem = JSON.parse(data);
+          newItem.id = Math.round(Math.random()*Math.pow(2, 32)).toString();
+          getStorage(ids, true)[newItem.id] = newItem;
+          return buildJsonResponse(newItem);
+        }
+      });
+    });
+
+    $httpBackend.whenPUT(new RegExp(baseUrlRe))
+    .respond(function(method, rawUrl, data, headers) {
+      return handle(rawUrl, data, headers, {
+        atItem: function(superIds, itemId) {
+          var storage = getStorage(superIds);
+          if (storage && storage[itemId]) {
+            var newItem = JSON.parse(data);
+            newItem.id = itemId;
+            storage[itemId] = newItem;
+            return buildJsonResponse(newItem);
+          }
+        }
+      });
+    });
+
+    $httpBackend.whenDELETE(new RegExp(baseUrlRe))
+    .respond(function(method, rawUrl, data, headers) {
+      return handle(rawUrl, data, headers, {
+        atItem: function(superIds, itemId) {
+          var storage = getStorage(superIds);
+          if (storage && storage[itemId]) {
+            delete storage[itemId];
+          }
+        }
+      });
+    });
+  };
+
+  ResourceMock.prototype = {
     subResourceMock: function(subUrl, subDataSource) {
       return new ResourceMock(this.baseUrl + '/?' + subUrl, subDataSource);
     },
