2457b923b3784eea6f756ef5946c9a808ee9bc27
David Simon <dsimon@americancouncils.org>
2013-10-29 10:45:39 -0400

    Added basicMock

---
+++ src/services/basicMock.js
@@ -0,0 +1,84 @@
+'use strict';
+
+angular.module('restfulNgMock')
+.factory('basicMock', [
+'$httpBackend',
+function($httpBackend) {
+  var HttpError = function(code, message) {
+    this.code = code;
+    this.message = message;
+  };
+
+  var DEFAULT_OPTIONS = {
+    debug: false
+  };
+
+  var BasicMock = function (base, options) {
+    this.base = base;
+    this.options = angular.extend({}, DEFAULT_OPTIONS);
+    this.setOptions(options || {});
+  };
+
+  BasicMock.prototype = {
+    _buildResponse: function(data) {
+      if (angular.isUndefined(data)) {
+        data = new HttpError(404, 'Not Found');
+      }
+
+      var responseInfo = { code: 200, message: 'OK' };
+      if (data instanceof HttpError) {
+        responseInfo.code = data.code;
+        responseInfo.message = data.message;
+        if (this.options.httpResponseInfoLabel) {
+          data = {};
+        } else {
+          data = responseInfo;
+        }
+      }
+
+      if (this.options.httpResponseInfoLabel) {
+        data[this.options.httpResponseInfoLabel] = responseInfo;
+      }
+
+      return [
+        responseInfo.code,
+        JSON.stringify(data),
+        { 'Content-Type': 'application/json' }
+      ];
+    },
+
+    route: function(method, pattern, func) {
+      var fullPattern = this.base + pattern;
+      var urlPattern = fullPattern
+        .replace('/', '\\/', 'g')
+        .replace('?', '([\\w\\-]+)');
+      var re = new RegExp( '^' + urlPattern  + '(?:/([\\w\\-]+))?(?:\\?.*)?$');
+
+      var me = this;
+      $httpBackend.when(method, re).respond(
+        function(method, rawUrl, body, headers) {
+          var url = purl(rawUrl, true);
+          var params = re.exec(url.attr('path')).slice(1);
+          var r = func(params, method, url, body, headers);
+          return me._buildResponse(r);
+        }
+      );
+    },
+
+    setOptions: function(opts) {
+      var me = this;
+      angular.forEach(opts, function(v, k) {
+        if (typeof DEFAULT_OPTIONS[k] === 'undefined') {
+          throw 'Invalid option key ' + k;
+        }
+        me.options[k] = v;
+      });
+      return me;
+    }
+  };
+
+  var BasicMockFactory = function(base, options) {
+    return new BasicMock(base, options);
+  };
+  return BasicMockFactory;
+}]);
--- test/unit/resourceMockSpec.js
+++ test/unit/resourceMockSpec.js
@@ -3,8 +3,9 @@
 describe('resourceMock', function () {
   beforeEach(module('restfulNgMock'));
 
-  var resourceMock, $http, $httpBackend;
-  beforeEach(inject(function(_resourceMock_, _$http_, _$httpBackend_) {
+  var basicMock, resourceMock, $http, $httpBackend;
+  beforeEach(inject(function(_basicMock_, _resourceMock_, _$http_, _$httpBackend_) {
+    basicMock = _basicMock_;
     resourceMock = _resourceMock_;
     $http = _$http_;
     $httpBackend = _$httpBackend_;
@@ -71,7 +72,30 @@ describe('resourceMock', function () {
     });
   });
 
-  describe('with a simple resource', function () {
+  describe('with a basic mock', function () {
+    var mock;
+    beforeEach(function() {
+      mock = basicMock('/foo');
+      mock.route('GET', '', function () {
+        return { foo: 'narf' };
+      });
+      mock.route('GET', '/bar/?', function (params) {
+        return { foo: 'bar' + params[0] };
+      });
+    });
+
+    it('responds on simple routes', function () {
+      grabHttpResult($http.get('/foo'));
+      expect(result.data).toEqual({ foo: 'narf' });
+    });
+
+    it('responds on parameterized routes', function () {
+      grabHttpResult($http.get('/foo/bar/123'));
+      expect(result.data).toEqual({ foo: 'bar123' });
+    });
+  });
+
+  describe('with a resource mock', function () {
     var books, booksMock;
     beforeEach(function() {
       books = {
